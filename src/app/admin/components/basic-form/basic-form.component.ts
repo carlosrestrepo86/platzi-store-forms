176cf3ae19d6b0dacafda567e4d091d3c4035700
Carlos Andres Restrepo <carres1234@gmail.com>
2025-08-19 14:51:49 -0500

    Primer commit

---
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-form',
+  templateUrl: './basic-form.component.html',
+  styleUrls: ['./basic-form.component.scss']
+})
+export class BasicFormComponent implements OnInit {
+
+  nameField = new FormControl('');
+  emailField = new FormControl('');
+  phoneField = new FormControl('');
+  colorField = new FormControl('');
+  dateField = new FormControl('');
+  ageField = new FormControl('');
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.nameField.valueChanges.subscribe(value => {
+      console.log(value);
+    })
+  }
+
+  getNameValue() {
+    console.log(this.nameField.value);
+  }
+
+}
